cb2ced35b2876f47174364fb7fd3e2699ad9a5b3
Cess <cessmbuguar@gmail.com>
2016-09-18 12:57:43 +0300

    countToken method implementation

---
+++ Split/src/split/CountMe.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package split;
+
+/**
+ *
+ * @author cessito
+ */
+import java.util.*;
+import java.util.Scanner;
+public class CountMe {
+    public static void main(String[] args){
+        Scanner anything = new Scanner(System.in);
+        System.out.println("You might wonna put something here");
+        
+        StringTokenizer st = new StringTokenizer(anything.nextLine());
+        System.out.println("Total tokens:" + st.countTokens());
+    }
+    
+}
