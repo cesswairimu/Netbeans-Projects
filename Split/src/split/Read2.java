56aec1938d361eb4cb5558f1c3738d403f842112
Cess <cessmbuguar@gmail.com>
2016-09-18 12:59:45 +0300

    hasMoreTokens method in java stringTokenizer clas'

---
+++ Split/src/split/Read2.java
@@ -0,0 +1,25 @@
+
+package split;
+
+/**
+ @author cessito
+ */
+import java.util.Scanner;
+import java.util.*;
+public class Read2 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter something here");
+        
+        StringTokenizer st = new StringTokenizer(input.nextLine());
+        
+        System.out.println("Total tokens :" + st.countTokens());
+       
+        //while  (st.hasMoreTokens()){
+          //  System.out.println("Next token :" + st.nextToken());
+            
+        }
+        
+    }
+    
+
