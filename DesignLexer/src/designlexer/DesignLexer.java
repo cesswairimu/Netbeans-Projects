400ed83196fc3f0fea3773b95502790cc7d4eee3
Cess <cessmbuguar@gmail.com>
2016-10-11 11:12:47 +0300

    storing keywords in an array and displaying them if input

--- DesignLexer/src/designlexer/DesignLexer.java
+++ DesignLexer/src/designlexer/DesignLexer.java
@@ -1,24 +1,36 @@
 
 package designlexer;
 
-/**
- *
- * @author cessito
- */
+
+import java.util.Arrays;
 import java.util.*; //allows input for scanner
 public class DesignLexer { //class 
     public static void main(String[] args) { //main method
     Scanner cess = new Scanner(System.in); // reading the scanner input
+    String [] keywords = {"abstract","boolean","goto", "const", "native", "volatile","break","byte","case", "catch",
+    "char", "class", "continue", "default", "do","double","else","extends","enum","false",
+    "final", "finally","float","for","if","implements","import", "instanceof", "int", "interface",
+    "long", "new", "null", "package", "public","private", "protected", "return"
+    , "short", "static", "super", "switch", "synchronized", "this", "throw", "throws",
+    "transient", "try", "true", "void", "while"};
     System.out.println("You might wonna type something here"); //prompting the user to input something
-    String str = cess.nextLine(); // storing the input(scanner) in a variable "str"
+    String str = cess.nextLine(); // storing the input(scanner) in a vacriable "str"
     StringTokenizer st = new StringTokenizer(str, " "); //spliting the input string using delimeter space
-   
+ 
     while (st.hasMoreTokens()){ //looping through the string
-        System.out.println(st.nextToken()); //output ing each token of the input string
+        String token = st.nextToken(); //output ing each token of the input string
+        if (Arrays.asList(keywords).contains(token)){
+            System.out.println(token + " is a keyword");
+            
+        }
+        else{
+       System.out.println("the word is " +token);
+       
+        
         
     }
 
-    
+   
     
        
     }
